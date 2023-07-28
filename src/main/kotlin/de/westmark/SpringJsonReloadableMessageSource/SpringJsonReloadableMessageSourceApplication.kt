ee13050df808c6b8656af12e1404f7ee3585a41b
Tim Westmark <westmark.tim@gmail.com>
2023-07-28 16:01:05 +0200

    Initital commit

---
+++ src/main/kotlin/de/westmark/SpringJsonReloadableMessageSource/SpringJsonReloadableMessageSourceApplication.kt
@@ -0,0 +1,11 @@
+package de.westmark.SpringJsonReloadableMessageSource
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJsonReloadableMessageSourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJsonReloadableMessageSourceApplication>(*args)
+}
---
+++ src/test/kotlin/de/westmark/SpringJsonReloadableMessageSource/SpringJsonReloadableMessageSourceApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.westmark.SpringJsonReloadableMessageSource
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJsonReloadableMessageSourceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
